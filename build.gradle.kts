0043862c0029c97208d79da8db3a33f737f930ff
dorohovgeorge <64730708+dorohovGeorge@users.noreply.github.com>
2024-02-20 00:46:54 +0300

    feat: add kotest and add some tests for future unit testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     runtimeOnly("org.postgresql:r2dbc-postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // https://mvnrepository.com/artifact/io.kotest/kotest-runner-junit5-jvm
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.projectreactor:reactor-test")
 }
 
