b0e25dca5cddbb8428c1064bb74f75e145df7383
dorohovgeorge <64730708+dorohovGeorge@users.noreply.github.com>
2024-05-29 22:56:39 +0300

    refactor: delete unused var

--- src/main/kotlin/souliving/backend/mapper/FormMappers.kt
+++ src/main/kotlin/souliving/backend/mapper/FormMappers.kt
@@ -14,25 +14,22 @@ import souliving.backend.model.locations.District
 import souliving.backend.model.locations.Metro
 
 var mapper: ObjectMapper = jacksonObjectMapper()
-fun PlainShortFormDto.toShortForm(): ShortFormDto {
-    val a = ShortFormDto(
-        id = this.id,
-        name = this.name,
-        age = this.age,
-        city = City(this.cityid, this.cityname!!),
-        district = District(this.districtid, this.districtname!!, this.districtcityid),
-        metro = parseMetro(this.metro),
-        budget = this.budget,
-        description = this.description,
-        dateMove = this.datemove,
-        properties = Properties(
-            this.propertiesid, this.smoking, this.alcohol, this.petfriendly!!, this.isclean!!, this.homeownerid
-        ),
-        photoId = this.photoid,
-        onlineDateTime = this.onlinedatetime
-    )
-    return a
-}
+fun PlainShortFormDto.toShortForm(): ShortFormDto = ShortFormDto(
+    id = this.id,
+    name = this.name,
+    age = this.age,
+    city = City(this.cityid, this.cityname!!),
+    district = District(this.districtid, this.districtname!!, this.districtcityid),
+    metro = parseMetro(this.metro),
+    budget = this.budget,
+    description = this.description,
+    dateMove = this.datemove,
+    properties = Properties(
+        this.propertiesid, this.smoking, this.alcohol, this.petfriendly!!, this.isclean!!, this.homeownerid
+    ),
+    photoId = this.photoid,
+    onlineDateTime = this.onlinedatetime
+)
 
 fun parseMetro(metroJson: String): List<Metro> {
     val model: List<Map<String, String>> = mapper.readValue(metroJson)
